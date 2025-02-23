5f3f222e0645d6eae04cbd7f0e0b1cdb20a4a212
Vishal Gupta <132651145+iamvishalgupta-oops@users.noreply.github.com>
2025-02-24 00:29:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+
+import About from './components/About';
+import Contact from './components/Contact';
+import Experience from './components/Experience';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Portfolio from './components/Portfolio';
+import SocialLink from './components/SocialLink';
+
+function App() {
+  return (<>
+  
+  <Navbar/>
+ 
+<Home/>
+<About/>
+<Portfolio/>
+<SocialLink/>
+<Experience/>
+<Contact/>
+</>
+  );
+}
+
+export default App;
